0f04df4c41ad2779819254d63a7a27cb64ef3d98
Harshan Andrews <harshanbandrews@yahoo.com>
2017-10-04 15:07:04 +0530

    Solution to problem Code: EZNO

---
+++ EasyNumbers.java
@@ -0,0 +1,14 @@
+class EasyNumbers{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of test cases: ");
+		int t = sc.nextInt();
+		for(int i = 1; i <= t; i++)
+		{
+			System.out.println("Enter the base and digits in base: ");
+			int b = sc.nextInt();
+			int d = sc.nextInt();
+			
+		}
+	}
+}
